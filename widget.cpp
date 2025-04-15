60b8497ee43cc9dfe8889c1720d57b0ada8bb8ea
EvgehaName <dubasov.eugeni@yandex.ru>
2025-04-15 21:37:59 +0300

    added test draw elements

--- widget.cpp
+++ widget.cpp
@@ -61,10 +61,10 @@ void Widget::initializeGL()
 
     float vertices[] = {
         //x    //y   //z
-        -0.5f, 0.5f, 0.0f,  // нижняя правая точка
-        0.5f, 0.5f, 0.0f,   // верняя правая точка
-        0.5f, -0.5f, 0.0f,  // верхняя левая точка
-        -0.5f, -0.5f, 0.0f  // нижняя левая точка
+        -1.5f, 1.5f, 0.0f,  // нижняя правая точка
+        1.5f, 1.5f, 0.0f,   // верняя правая точка
+        1.5f, -1.5f, 0.0f,  // верхняя левая точка
+        -1.5f, -1.5f, 0.0f  // нижняя левая точка
     };
 
 
@@ -111,21 +111,26 @@ void Widget::paintGL()
     QMatrix4x4 projection;
     projection.perspective(45.0f, width() / float(height()), 0.1f, 100.0f);
 
-    QMatrix4x4 view = m_actor->camera()->viewMatrix();
-    view.translate(0,0,-3.0f);
+    QMatrix4x4 view; //= m_actor->camera()->viewMatrix();
+    view.translate(0,0,-25.0f);
     //qDebug() << view;
     //view.translate(0, 0, -3.0f);
 
-    QMatrix4x4 model;
-    model.rotate(0.0f, 0.0f, 0.0f);
-
-    QMatrix4x4 mvp = projection * view * model;
-    m_program->setUniformValue("mvp", mvp);
-
-
-    glBindVertexArray(m_vao);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
+    // test
+    for (size_t i = 0; i <= 5; i++)
+    {
+        QMatrix4x4 modelElem;
+        modelElem.translate(i * 3.1f,0.0f, 0.0f);
+        if(i > 2)
+        {
+            modelElem.translate(0.0f, 0.0f, i * 1.0f);
+            modelElem.rotate(i * 90.0f, 0.0f, 1.0f, 0.0f);
+        }
+        QMatrix4x4 mvp = projection * view * modelElem;
+        m_program->setUniformValue("mvp", mvp);
+        glBindVertexArray(m_vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);     
+    }
     m_program->release();
 }
 
