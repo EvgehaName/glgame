cfce076edf165b8c0cc22acb4f420db2587ad85d
Denis Eremenko <mamayma8@gmail.com>
2025-04-21 23:41:50 +0300

    Оптимизация с целью предотвращения утечки, т.к. камера не удалялась

--- headers/actor.h
+++ headers/actor.h
@@ -24,7 +24,7 @@ private:
     float m_lookFactor = 1.0f;
     float m_mouseSensScale = 1.0f;
     QVector3D m_position;
-    CameraBase * m_camera;
+    FPSCamera m_camera;
 };
 
 #endif // ACTOR_H
